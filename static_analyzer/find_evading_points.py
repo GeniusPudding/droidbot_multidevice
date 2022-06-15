0e27cbd29954ed08850f90deb83ec502142771c7
GeniusPudding <r08922107@ntu.edu.tw>
2022-06-15 15:37:11 +0800

    fix param type,list; invoke, branch_logger; v16

--- static_analyzer/find_evading_points.py
+++ static_analyzer/find_evading_points.py
@@ -15,10 +15,10 @@ from commons import *
 from rename import debug_rename
 import subprocess
 apk_dir = 'C:\\Users\\user\\Desktop\\testing\\dataset\\runnable_on_android6\\TriggerZoo_antiemulator'
-diff_dir = 'C:\\Users\\user\\Desktop\\testing\\dataset\\diffs'
+diff_dir = 'C:\\Users\\user\\Desktop\\testing\\dataset\\diffs2'
 ag_dir = 'C:\\Users\\user\\Desktop\\droidbot_multidevice\\static_analyzer'
-log_path = 'C:\\Users\\user\\Desktop\\testing\\dataset\\\method_seq_logs\\RealJ6+_SamsungGalaxyS10\\TriggerZoo_antiemulator_withend'
-
+log_path = 'C:\\Users\\user\\Desktop\\testing\\dataset\\\method_seq_logs\\RealJ6+_SamsungGalaxyS10\\TriggerZoo_antiemulator_withtargets'
+entry_list = ["onCreate", "onStart", "onStartCommand","onResume", "onReStart", "onPause", "onStop", "onDestroy", "onTouch", "onReceive"]
 def get_analysis(apk_path):#init for apk analysis
     d = DalvikVMFormat(APK(apk_path))
     a = Analysis(d)
@@ -84,6 +84,7 @@ def gen_method_json(method_analysis,method_sign,from_callee_method):
     method_jsonObj['method_ins']['blocks'] = {}
     method_jsonObj['method_ins']['ins2block'] = {}    
     method_jsonObj['sign'] = method_sign
+    method_jsonObj['callee'] = from_callee_method
 
     # Basic blocks, CFG info in a method:
     all_bb_relations_in_method = {'tochilds':{},'toparents':{}}
@@ -135,51 +136,62 @@ def find_analysisobj_in_apitree(method_sign,api_tree):
 
     return method_analysis
 
-def gen_cfg_info(logs,evading_index,parent_index):
-        line = logs[evading_index]
-        line = line[line.index('L'):].strip()
-        print(f'evading_index:{evading_index},parent_index:{parent_index}')
-        if line[line.index(';->')+3:line.index('(')] in entry_list:#entry methods have no parent method
-            #continue
-            return None
-            #現階段可能會有很多沒抓到的因為有看到TriggerMethod直接接一個onCreate()的，可能要再log更精細
-        parent_line = logs[parent_index]
-        parent_line = parent_line[parent_line.index('L'):].strip()
-        method_analysis = find_analysisobj_in_apitree(parent_line,api_tree)
-        callees = method_analysis.xrefto
-        callee_signs = [get_MethodAnalysis_signature(methodobj) for _, methodobj, _ in callees]     
+def gen_cfg_info(logs,evading_index,parent_index,api_tree):
+    line = logs[evading_index]
+    line = line[line.index('L'):].strip()
+    #print(f'evading_index:{evading_index},parent_index:{parent_index}')
+    if line[line.index(';->')+3:line.index('(')] in entry_list:#entry methods have no parent method
+        #continue
+        return None
+        #現階段可能會有很多沒抓到的因為有看到TriggerMethod直接接一個onCreate()的，可能要再log更精細
+    parent_line = logs[parent_index]
+    parent_line = parent_line[parent_line.index('L'):].strip()
+    method_analysis = find_analysisobj_in_apitree(parent_line,api_tree)
+    callees = method_analysis.xrefto
+    callee_signs = [get_MethodAnalysis_signature(methodobj) for _, methodobj, _ in callees]     
 
-        #input(f'callee_signs:{callee_signs},line:{line}')
-        if line not in callee_signs:
-            #continue
-            return None
-            #現階段可能會有很多沒抓到的因為有看到TriggerMethod直接接一個onCreate()的，可能要再log更精細
-        input(f'Evading Points!')
-        method_obj = gen_method_json(method_analysis,parent_line,line)
-        return method_obj
+    #input(f'callee_signs:{callee_signs},line:{line}')
+    if line not in callee_signs:
+        #continue
+        return None
+        #現階段可能會有很多沒抓到的因為有看到TriggerMethod直接接一個onCreate()的，可能要再log更精細
+    #input(f'Evading Points!')
+    method_obj = gen_method_json(method_analysis,parent_line,line)
+    return method_obj
 def get_evading_point(method_jsonObj):#TODO 需要實際evading案例來Debug
+    #print(f'method_jsonObj:{method_jsonObj}')
     block_offset = method_jsonObj['from_offset'] 
-    d = method_jsonObj['cfg']['toparent']
+    d = method_jsonObj['cfg']['toparents']
     if d[block_offset] == []:
-        input('Start diverge before 0x0 block, why?')
+        print('Start diverge before 0x0 block, why?')
+        return None #在最初的basic block就發生
     else:
         branch_ins = method_jsonObj['method_ins']['blocks'][d[block_offset][0]][-1]
-        input(branch_ins)
+        #input(f'Last branch:{branch_ins}, offset:{d[block_offset][0]}')
         if 'if-' not in branch_ins:
-            input('Why')
+            s = method_jsonObj['sign']
+            c = method_jsonObj['callee']
+            #print(f'branch_ins:{branch_ins},method:{s},callee:{c}')
+            #input('Why')
+            return None
+        return branch_ins, d[block_offset][0],method_jsonObj['sign'], method_jsonObj['callee']
     #method_jsonObj['method_ins']['blocks'] = {}
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    diff_name = sys.argv[1]
+    # diff_name = sys.argv[1]
+def main(diff_name,p2f):
+    
     index = int(diff_name.split('_')[-2])
     package_name = diff_name[:-11]#be careful of over 10 diffs for a same package
-    sess_name =  "androguard_session.ag"  # package_name + '_session.ag'
+    #sess_name =  "androguard_session.ag"  # package_name + '_session.ag'
+
     #diff map to apk
-    with open('../jsons/packagename2filename.json','r') as f:
-        p2f = json.load(f)
+
+    if package_name not in p2f:#TODO json內感覺缺了一些 待修正
+        return None, None
     apk_name = p2f[package_name] + '.apk'
     apk_path = os.path.join(apk_dir,apk_name)
-
+    print(f'diff_name:{diff_name},apk_name:{apk_name}')
     #建表讓每一個API的分析類別都能被快速被查找
     api_tree = gen_API_analysis_tree(apk_path)
 
@@ -208,14 +220,19 @@ if __name__ == '__main__':
     call_stack = ['entry']
     for i,line in enumerate(er):
         if line.startswith('START:'):
+            # if len(call_stack) == 0:
+            #     input(f'line:{line}')
             emu_parent_index[i] = call_stack[-1]    
             call_stack.append(i)
         elif line.startswith('END:'):
+            if len(call_stack) == 1:#怎麼沒有START就出現END 可能I/O又有問題
+                #input(f'line:{line},last:{er[i-1]}')
+                return None, None
             call_stack = call_stack[:-1]
             emu_parent_index[i] = call_stack[-1] 
 
 
-    print(f'real_parent_index:{real_parent_index}, emu_parent_index:{emu_parent_index}')
+    #print(f'real_parent_index:{real_parent_index}, emu_parent_index:{emu_parent_index}')
     #針對diff出來差異的那幾項，整理出evading point的index
     real_evading_index = []
     emu_evading_index = []
@@ -242,36 +259,54 @@ if __name__ == '__main__':
                     emu_evading_index.append(int(line_index[1].split(',')[0])-1)   #start line in emu 
             except:
                 raise ValueError(f'diff line error:{diff_line}')
-    print(f'real_evading_index:{real_evading_index},emu_evading_index:{emu_evading_index}')
-    print(f'apk_name:{apk_name}')
+    #print(f'real_evading_index:{real_evading_index},emu_evading_index:{emu_evading_index}')
+    #print(f'apk_name:{apk_name}')
     #然後在API序列中查找目標並生出與他相關的CFG資訊
     evading_points = []
-    entry_list = ["onCreate", "onStart", "onStartCommand","onResume", "onReStart", "onPause", "onStop", "onDestroy", "onTouch", "onReceive"]
+    
     for i in real_evading_index:
         if i == 'gap':
             continue
         p_i = real_parent_index[i]
         if p_i == 'entry':#p_i == 'index'是原因不明的差異
             continue
-        method_obj = gen_cfg_info(rr,i,p_i) 
+        method_obj = gen_cfg_info(rr,i,p_i,api_tree) 
         if not method_obj:
             continue   
         #然後抓出branch的位子 
-        get_evading_point(method_obj)
-        input(f'p_i:{p_i}\nmethod_obj:{method_obj}')
+        ep = get_evading_point(method_obj)
+        #input(f'p_i:{p_i}\nmethod_obj:{method_obj},ep:{ep}')
+        if ep:
+            evading_points.append(ep)
     for i in emu_evading_index:
         if i == 'gap':
             continue
         p_i = emu_parent_index[i]
         if p_i == 'entry':
             continue
-        method_obj = gen_cfg_info(er,i,p_i)  
+        method_obj = gen_cfg_info(er,i,p_i,api_tree)  
         if not method_obj:
             continue  
-        input(f'p_i:{p_i}\nmethod_obj:{method_obj}')
-        get_evading_point(method_obj)
+        #input(f'p_i:{p_i}\nmethod_obj:{method_obj},ep:{ep}')
+        ep = get_evading_point(method_obj)
+        if ep:
+            evading_points.append(ep)
     #抓出那些diverse line上面一個block的分支(若存在)，輸出並儲存所有的evading points位置 (method, block)   
     output_dir = 'C:\\Users\\user\\Desktop\\droidbot_multidevice\\evading_points'
+
+
+    return apk_name, evading_points
+
+if __name__ == '__main__':
+    with open('../jsons/packagename2filename.json','r') as f:
+        p2f = json.load(f)
+    i = 0
+    for diff_name in os.listdir(diff_dir):
+        if os.path.getsize(os.path.join(diff_dir,diff_name)) == 0 or not diff_name[-9:] == '_diff.txt':
+            continue
+        i += 1
+        apk_name, evading_points = main(diff_name,p2f)
+        print(f'{i}-th apk_name:{apk_name}, evading_points:{evading_points}')
 
 
 
